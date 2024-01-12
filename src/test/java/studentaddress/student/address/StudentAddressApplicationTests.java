b6aaac340df3495a1247e337373725c31800b640
iamramyak <iamramyak7@gmail.com>
2024-01-12 16:04:21 +0530

    student address

---
+++ src/test/java/studentaddress/student/address/StudentAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package studentaddress.student.address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
